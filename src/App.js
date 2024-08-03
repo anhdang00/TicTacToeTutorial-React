db3b57ed607cf6dae7fd1622d2dfa2693f290c1a
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2024-08-03 12:24:24 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+    </>
+  );
+}
