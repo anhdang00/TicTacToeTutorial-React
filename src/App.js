65446b35e0e32a059c1c4749bf1365bde2827fd6
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2024-08-03 20:09:58 +0000

    add caculateWinner() func

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(idx) {
+    if (squares[idx] || calculateWinner(squares)){
+      return;
+    }
     if (squares[idx]){
       return;
     }
@@ -45,4 +48,25 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++){
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
