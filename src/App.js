a02d0658a57a0ed9f4270926423283dbb9837bee
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2024-08-03 20:17:43 +0000

    display game status

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ export default function Board() {
 
   return (
     <>
+      <div className='status'>{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
