2f0fdcc12f9045dad23e07febab0b20f10407b17
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2024-08-03 20:30:18 +0000

    add props to track players and game history

--- src/App.js
+++ src/App.js
@@ -58,6 +58,9 @@ function Board() {
 }
 
 export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState(Array(9).fill(null));
+
   return (
     <div className='game'>
       <div className='game-board'>
