5fc9415bedb990a167270a34484a8ccbf9d13ce2
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2024-08-03 21:30:07 +0000

    update game history when a move is made

--- src/App.js
+++ src/App.js
@@ -57,15 +57,18 @@ export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove+1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length -  1);
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
-
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
