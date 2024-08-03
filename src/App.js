e762c6a6a55851de30bb15245460e88fb6017060
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2024-08-03 20:25:28 +0000

    add Game() function

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -55,6 +55,10 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+export default function Game(){
+  
 }
 
 function calculateWinner(squares){
