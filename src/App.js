5af48f84c1f48e533305a473596b6549d08974cb
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2024-08-03 20:16:08 +0000

    announce winner

--- src/App.js
+++ src/App.js
@@ -29,6 +29,14 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner){
+    status = "Winner: " + winner;
+  }else{
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
       <div className="board-row">
