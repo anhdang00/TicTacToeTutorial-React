3e04885d61486c2187f012f55467e0dfb8b735bd
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2024-08-03 21:25:10 +0000

    create component to keep track of the current move being reviewed

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ function Board({xIsNext, squares, onPlay}) {
 export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
