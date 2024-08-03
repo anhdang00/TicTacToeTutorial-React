065b5ec77dc1631876a300ac890dca451e6d1f57
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2024-08-03 20:32:06 +0000

    track the square currently being played

--- src/App.js
+++ src/App.js
@@ -60,6 +60,7 @@ function Board() {
 export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState(Array(9).fill(null));
+  const currentSquares = history[history.length - 1];
 
   return (
     <div className='game'>
